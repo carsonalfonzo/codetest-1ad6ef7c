7e48b4d08d5881e65ed8756d022bf94688d8fbc4
Alfonzo <carsonalfonzo@hotmail.com>
2016-08-17 11:12:23 -0600

    Initial CheckIn. Created BDD Tests using SpecFlow and initial class library design.

---
+++ ImagineX.TenSubString/ImagineX.TenSubstring/TenSubstringCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagineX.TenSubstring
+{
+    public class TenSubstringCalculator
+    {
+        public int InputNumber { get; set; }
+        public List<int> Generate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
